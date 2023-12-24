6f53c61927ae5b867e63fbd88d0c30fe741bf18f
haldun <haldunbucak97@outlook.com>
2023-12-24 18:10:16 +0100

    changed cover to marching squares

---
+++ assets/scripts/cover-bg/sketch.js
@@ -0,0 +1,43 @@
+// Weight and Height, 
+let w, h;
+// Rows and Columns
+let rows, columns;
+// Size of Cells
+let size;
+// Offsets for Website
+let HEADER_OFFSET;
+// Scalar Field
+let field;
+
+let canvas;
+
+function setup() {
+	// Calculate window sizes
+	HEADER_OFFSET = 0;
+  w = windowWidth;
+  h = 800 - HEADER_OFFSET;
+
+	size = 50;
+
+	// Calculate the row and column size
+	rows = int(h / size) + 1;
+	columns = int(w / size) + 1;
+
+	// Initialize the Scalar Field
+	field = new ScalarField(rows, columns, size, 0.2, 0.002, 50);
+
+  canvas = createCanvas(w, h);
+	canvas.style("z-index", '-1');
+	canvas.position(0,0);
+}
+
+function draw() {
+  // put drawing code here
+  background("#3C2C4F");
+	field.draw(true);
+	push();
+	noStroke();
+	fill(0, 80);
+	rect(0,0, w, h);
+	pop();
+}
